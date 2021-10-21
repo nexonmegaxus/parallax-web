846b91b83207042cf3fc2af304c9942e3304d5ba
Jossia Elang <81104672+nexonmegaxus@users.noreply.github.com>
2021-10-21 09:29:18 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$('.page-scroll').on('click', function(e) {
+
+  var tujuan = $(this).attr('href');
+ 
+  var elemenTujuan = $(tujuan);
+ 
+  $('html , body').animate({
+   scrollTop: elemenTujuan.offset().top - 50
+  });
+ 
+  e.preventDefault();
+ });
+
+ // PARALLAX
+
+// about
+$(window).on('load',function() {
+  $('.pKiri').addClass('pMuncul')
+  $('.pKanan').addClass('pMuncul')
+  });
+
+ $(window).scroll(function() {
+  var wScroll = $(this).scrollTop();
+  //jumbotron
+  $('.jumbotron img').css({
+    'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+  });
+
+  $('.jumbotron h1').css({
+    'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+  });
+
+  $('.jumbotron p').css({
+    'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+  });
+
+  // portfolio
+  if( wScroll > $('.portfolio').offset().top - 800) {
+    $('.portfolio .thumbnail').each(function(i){
+      setTimeout(function(){
+        $('.portfolio .thumbnail').eq(i).addClass('muncul');
+      }, 400 * (i+1));
+    });
+    
+  }  
+});
